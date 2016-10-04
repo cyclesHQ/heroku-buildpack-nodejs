235380240c74971434b6ff63c85e7f56d2771b21
pburdelev <burdelevpasha@gmail.com>
2016-10-04 15:32:33 +0600

    Update environment.sh

--- lib/environment.sh
+++ lib/environment.sh
@@ -23,7 +23,10 @@ export_env_dir() {
     local whitelist_regex=${2:-''}
     local blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH|LANG)$'}
     if [ -d "$env_dir" ]; then
+      echo "---------asd-------"
+      echo "$env_dir"
       for e in $(ls $env_dir); do
+        echo "$e"
         echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
         export "$e=$(cat $env_dir/$e)"
         :
